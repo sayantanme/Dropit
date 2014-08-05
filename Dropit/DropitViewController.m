b071530608aaba000149c0d1ecaa3a245e2b1f65
Sayantan Chakraborty <sayantanme@gmail.com>
2014-08-05 17:40:59 +0530

    Added a view where random coloured blocks appears when clicked in the view

--- Dropit/DropitViewController.m
+++ Dropit/DropitViewController.m
@@ -9,10 +9,49 @@
 #import "DropitViewController.h"
 
 @interface DropitViewController ()
+@property (weak, nonatomic) IBOutlet UIView *gameView;
 
 @end
 
 @implementation DropitViewController
+
+static const CGSize DROP_SIZE = {40 , 40};
+
+- (IBAction)tap:(UITapGestureRecognizer *)sender
+{
+    [self drop];
+}
+
+-(void) drop
+{
+    CGRect frame;
+    frame.origin = CGPointZero;
+    frame.size = DROP_SIZE;
+    int x = (arc4random()%(int)self.gameView.bounds.size.width)/DROP_SIZE.width;
+    frame.origin.x = x*DROP_SIZE.width;
+    
+    UIView *dropView =[[UIView alloc]initWithFrame:frame];
+    dropView.backgroundColor = [self randomColor];
+    [self.gameView addSubview:dropView];
+}
+
+-(UIColor *)randomColor
+{
+    switch (arc4random()%5) {
+        case 0:return [UIColor greenColor];
+            break;
+        case 1:return [UIColor redColor];
+            break;
+        case 2:return [UIColor blueColor];
+            break;
+        case 3:return [UIColor orangeColor];
+            break;
+        case 4:return [UIColor blueColor];
+            break;
+        
+    }
+    return [UIColor blackColor];
+}
 
 - (void)viewDidLoad
 {
